75cc91381efbab3a89999dc57d6337d09c7c4f6f
Jared <kuil5230@gmail.com>
2019-07-25 22:18:44 +0800

    .

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,10 @@
-import React, { useReducer } from "react";
+import React, { lazy, Suspense, useEffect, useReducer } from "react";
 import "components/TodoList/index.css";
-import { List } from "antd";
+import { List, Spin } from "antd";
 
 import doToListReducer from "components/TodoList/reducer";
-import TodoItem from "components/TodoItem";
 import TodoFrom from "components/TodoForm";
+const TodoItem = lazy(() => import("components/TodoItem"));
 
 const data = [
   { title: "項目1", content: "內容1內容1內容1內容1內容2", date: "2019-07-16", done: false },
@@ -12,26 +12,33 @@ const data = [
 ];
 
 const TodoList = () => {
-  const [todo, dispatch] = useReducer(doToListReducer, { todoList: data });
+  const localStorageTodoListData = JSON.parse(localStorage.getItem("todo"));
+  const [todo, dispatch] = useReducer(doToListReducer, { todoList: localStorageTodoListData || data });
+
+  useEffect(() => {
+    localStorage.setItem("todo", JSON.stringify(todo.todoList));
+  }, [todo.todoList]);
 
   return (
     <>
       <TodoFrom todoItemValue={item => dispatch({ type: "ADD_ITEM", addTodoListItem: item })} />
-      <List
-        bordered
-        locale={{ emptyText: "目前無代辦事項" }}
-        dataSource={todo.todoList}
-        renderItem={(item, index) => (
-          <TodoItem
-            todoDoneState={item.done}
-            markTodoDone={() => dispatch({ type: "DONE_ITEM", doneItem: index })}
-            removeTodoItem={() => dispatch({ type: "REMOVE_ITEM", removeItem: index })}
-            editTodoItemText={item => dispatch({ type: "EDIT_ITEM", editIndex: index, editItem: item })}
-            todoItemDataSource={item}
-            key={index}
-          />
-        )}
-      />
+      <Suspense fallback={<Spin />}>
+        <List
+          bordered
+          locale={{ emptyText: "目前無代辦事項" }}
+          dataSource={todo.todoList}
+          renderItem={(item, index) => (
+            <TodoItem
+              todoDoneState={item.done}
+              markTodoDone={() => dispatch({ type: "DONE_ITEM", doneItem: index })}
+              removeTodoItem={() => dispatch({ type: "REMOVE_ITEM", removeItem: index })}
+              editTodoItemText={item => dispatch({ type: "EDIT_ITEM", editIndex: index, editItem: item })}
+              todoItemDataSource={item}
+              key={index}
+            />
+          )}
+        />
+      </Suspense>
     </>
   );
 };
