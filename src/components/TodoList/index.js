8390e6928e040174426b8e5659f5414782525eff
Jared <kuil5230@gmail.com>
2019-07-26 00:11:18 +0800

    .

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense, useEffect, useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import "components/TodoList/index.css";
-import { List, Spin } from "antd";
+import { List } from "antd";
 
 import doToListReducer from "components/TodoList/reducer";
 import TodoFrom from "components/TodoForm";
-const TodoItem = lazy(() => import("components/TodoItem"));
+import TodoItem from "components/TodoItem";
 
 const data = [
   { title: "項目1", content: "內容1內容1內容1內容1內容2", date: "2019-07-16", done: false },
@@ -22,23 +22,21 @@ const TodoList = () => {
   return (
     <>
       <TodoFrom todoItemValue={item => dispatch({ type: "ADD_ITEM", addTodoListItem: item })} />
-      <Suspense fallback={<Spin />}>
-        <List
-          bordered
-          locale={{ emptyText: "目前無代辦事項" }}
-          dataSource={todo.todoList}
-          renderItem={(item, index) => (
-            <TodoItem
-              todoDoneState={item.done}
-              markTodoDone={() => dispatch({ type: "DONE_ITEM", doneItem: index })}
-              removeTodoItem={() => dispatch({ type: "REMOVE_ITEM", removeItem: index })}
-              editTodoItemText={item => dispatch({ type: "EDIT_ITEM", editIndex: index, editItem: item })}
-              todoItemDataSource={item}
-              key={index}
-            />
-          )}
-        />
-      </Suspense>
+      <List
+        bordered
+        locale={{ emptyText: "目前無代辦事項" }}
+        dataSource={todo.todoList}
+        renderItem={(item, index) => (
+          <TodoItem
+            todoDoneState={item.done}
+            markTodoDone={() => dispatch({ type: "DONE_ITEM", doneItem: index })}
+            removeTodoItem={() => dispatch({ type: "REMOVE_ITEM", removeItem: index })}
+            editTodoItemText={item => dispatch({ type: "EDIT_ITEM", editIndex: index, editItem: item })}
+            todoItemDataSource={item}
+            key={index}
+          />
+        )}
+      />
     </>
   );
 };
